9ebeb29def4535ed6f377e6f83c4248127221014
Aleksandr <mivotanaj@gmail.com>
2022-12-26 12:13:56 +0100

    weather with telegBot

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-#  КАЛЬКУЛЯТОР 2.0
-import numexpr
-from colorama import init
-from colorama import Fore, Back, Style
-init()
-
-print(Fore.CYAN)
-
-expr = input("Введите математическое выражение: ")
-
-result = numexpr.evaluate(expr)
+    from aiogram import Bot, Dispatcher, executor, types
+    import python_weather
 
 
-print(Fore.GREEN)
+    #bot init
+    bot = Bot(token="")
+    dp = Dispatcher(bot)
+    async with python_weather.Client(format=python_weather.IMPERIAL) as client:
 
-print(f"Результат: {result}")
+    # echo
+    @dp.message_handler()
+    async def echo(message: types.Message):
+        weather = await client.get(message.text)
+        celsius = round((weather.current.temperature - 32) / 1.8)
 
+        resp_msg = f"Текущая температура: {celsius}°"
+
+        if celsius <= 10:
+            resp_msg += "\n\nПрохладно! Одевайтесь потеплее!"
+        else:
+            resp_msg += "\n\nТепло! Одевайтесь полегче!"
+
+        await message.answer(resp_msg)
+
+    # run long-polling
+    if __name__ == "__mail__":
+        executor.start_polling(dp, skip_updates=True)
